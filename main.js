5897ae0318fa35e7bece935e0dbf8ab297e074d8
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-02-16 23:18:32 -0700

    completed the css for singup section

---
+++ main.js
@@ -0,0 +1,23 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slides-item");
+//   var dots = document.getElementsByClassName("slides-dots");
+
+//   slideIndex = slides.length
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace("active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += "active";
+// }
